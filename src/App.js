da5edfb13e93c6e6600639693c4a44c48de4e608
Zidan <midoz393@gmail.com>
2019-12-14 18:56:51 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
+import ReactDOM from 'react-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      quote: '',
+      author: ''
+    }
+
+    this.newQuote = this.newQuote.bind(this);
+  }
+  
+  async random() {
+    const response = await fetch('https://api.quotable.io/random')
+    const data = await response.json()
+    this.setState({quote: data.content, author: data.author})
+  }
+
+  newQuote() {
+    this.random()
+  }
+
+  componentDidMount(){
+    this.random()
+  }
+
+  render(){
+    return (
+      <div id="quote-box">
+        <div id='text'>
+          {this.state.quote}
+        </div>
+        <div id='author'>
+          {this.state.author}
+        </div>
+        <div id="new-quote">
+          <button onClick={this.newQuote}>New Quote</button>
+        </div>
+        <div>
+          <a id="tweet-quote" href={"https://twitter.com/intent/tweet?text="+encodeURIComponent(this.state.quote)+' -- ' + encodeURIComponent(this.state.author)}>Tweet this</a>
+        </div>
+      </div>
+    );
+  }
 }
 
+
+ReactDOM.render(<App />, document.getElementById('root'))
 export default App;
